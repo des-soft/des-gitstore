8c42cb0afec7f4196ca225ea86a3862c2ba88614
eczn <1975845422@qq.com>
2018-04-11 21:01:18 +0800

    [+] repo cline, push, pull, add, commit, and so on

--- src/Gitstore.js
+++ src/Gitstore.js
@@ -1,29 +1,100 @@
-const { EventEmitter } = require('events'); 
-const fs = require('then-fs'); 
-const simpleGit = require('simple-git'); 
+const { EventEmitter } = require('events')
+    , fs = require('then-fs')
+    , simpleGit = require('simple-git')
+    , simpleGitPromisify = require('./simple-git-promisify')
+    , rimraf = require('rimraf-then')
+    , mkdir = require('./utils/mkdir')
+    , pathPreset = require('./utils/pathPreset')
+// 依赖 
 
 module.exports = class Gitstore extends EventEmitter {
-    constructor(git_base, git_uri){
+    /**
+     * @description 构造函数
+     * @param { String } git_base git 文件夹
+     * @param { String } git_uri git uri 
+     */
+    constructor(git_base, git_uri) {
         super(); 
-      
+        // git_uri: 
+        // https://${user}:${pwd}@${git_path}
+
+        // 目标文件夹前置一个 __DES_STORE__ 用于标识 
+        git_base = pathPreset(git_base, '__DES_STORE__'); 
+
         this.git_base = git_base; 
         this.git_uri  = git_uri; 
-        this.git      = simpleGit(this.git_base); 
-        this.ready    = this.init(); 
+        mkdir(this.git_base); 
+        
+        // simple-git 实例 
+        this.git = simpleGit(this.git_base);
+        // 对实例进行 Promisify 
+        simpleGitPromisify(
+            this.git, 
+            [
+                'checkIsRepo', 'raw', 'pull', 
+                'commit', 'push', 'add'
+            ]
+        ); 
+        this.ready = this.init(); 
     }
 
-    async init(){
-        let isExists = await fs.exists(this.git_base)
-        
-        if (isExists){
-            // 存在 
-            
+    /**
+     * @description just origin pull 
+     * @returns { Promise<PullSummary> }
+     */
+    async pull(){
+        await this.ready; 
+
+        return this.git.pull$(
+            'origin',
+            'master'
+        ); 
+    }
+
+    /**
+     * @description just commit & push to origin master 
+     * @param { String } msg 
+     * @returns { Promise }
+     */
+    async commitPush(msg = 'No Msg'){
+        // git add *
+        await this.git.add$(['*'])
+        // git commit -m ${msg}
+        await this.git.commit$(msg); 
+
+        // git push origin master 
+        return this.git.push$(
+            'origin', 'master'
+        ); 
+    }
+
+    /**
+     * @description init 初始化仓库 
+     * @returns { Promise } 
+     */
+    async init() {
+        if (this.ready) return this.ready; 
+
+        let isRepo = await this.git.checkIsRepo$(); 
+
+        if (isRepo) {
+            // 因为 this.cloneRepo() 本身蕴含的值就是 null 
+            // 为保证一致性，此处也是 null 
+            return Promise.resolve(null); 
         } else {
-            return this.clone(); 
+            return this.cloneRepo(); 
         }
     }
 
-    async clone(){
-
+    /**
+     * @description git clone ${git_uri} ${git_base} 
+     * @returns { Promise }
+     */
+    cloneRepo() {
+        return this.git.raw$([
+            `clone`,
+            this.git_uri,
+            this.git_base 
+        ]); 
     }
 }
